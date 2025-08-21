699d688c72e3f730562f64d12ce86d8609289c3e
jimuel <jimuelcinco.work@gmail.com>
2025-08-21 12:49:11 +0800

    Fix hero min heighjt

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -8,4 +8,28 @@ import { TechStackComponent } from '../tech-stack/tech-stack.component';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  private resizeListener = () => this.adjustHeroHeight();
+
+  ngOnInit(): void {
+    this.adjustHeroHeight();
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.resizeListener); // Clean up to avoid memory leaks
+  }
+
+  adjustHeroHeight() {
+    const navbar = document.getElementById('navbar');
+    const hero = document.getElementById('hero');
+
+    if (navbar && hero) {
+      const navbarHeight = navbar.offsetHeight;
+      const viewportHeight = window.innerHeight;
+
+      const heroMinHeight = viewportHeight - navbarHeight;
+
+      hero.style.minHeight = `${heroMinHeight}px`;
+    }
+  }
+}
